010899361f4d7b06df83edfc19b2265f8ed74198
boris-isac <borisisac@gmail.com>
2025-12-02 13:51:06 +0000

    [ADD]  main cointent and connected

---
+++ client/src/components/MainContent/MainCintent.tsx
@@ -0,0 +1,15 @@
+import './MainCintent.css'
+
+function MainContent() {
+    return (
+        <>
+            <div className="conteiner">
+                <h1>Exclusive Private Parties</h1>
+                <p>bskdhujfbskdufbis usdbf isudbfiksudbf isdubf isdufb sidubfisud</p>
+            </div>
+            
+        </>
+    )
+}
+
+export default MainContent
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import DarkNavBar from "../components/Navbar/NavBar";
+import MainContent from "../components/MainContent/MainCintent";
 
 
 function Home() {
     return (
         <>
-        <DarkNavBar />
+            <DarkNavBar />
+            <MainContent />
         </>
         
     )
