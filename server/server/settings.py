2afa1bcbb3e85af287d46a31d08403de853d7cf1
boris-isac <borisisac@gmail.com>
2025-12-13 19:30:23 +0000

    [ADD] psql configurations

--- server/server/settings.py
+++ server/server/settings.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from pathlib import Path
 
@@ -73,9 +77,13 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("PSQL_DB"),
+        "USER": os.environ.get("PSQL_USER"),
+        "PASSWORD": os.environ.get("PSQL_USER_PASSWORD"),
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
