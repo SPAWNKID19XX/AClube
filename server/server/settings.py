bb4b3aa813e637850497364b02650eac91cf3647
boris-isac <borisisac@gmail.com>
2025-12-19 08:55:26 +0000

    [UPDATE] translations

--- server/server/settings.py
+++ server/server/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qbv4x#%b666pdkel_o3pi4pe*(pock0yav-d@4&7sx^vhc+(v&'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
