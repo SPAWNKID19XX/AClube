62276dd27964a1f7bc97c008c118265255915091
boris-isac <borisisac@gmail.com>
2025-12-27 17:05:51 +0000

    [ADD]  signup form front

--- client/src/components/LoginForm/LoginForm.tsx
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -1,25 +1,87 @@
-import './LoginForm.css';
-import { useTranslation } from "react-i18next";
+import './LoginForm.css'
+import {useState} from "react";
+import axios from "axios";
+import {useTranslation} from "react-i18next";
 
 
 
-
-
-
-function LoginForm() {
-
-    const {t} = useTranslation()
-    return (
-        <>
-            <form>
-                <h1>Form Login</h1>
-            </form>
-        
-        
-        
-        </>
-    )
+interface LoginForm {
+    username: string;
+    password: string;
 }
 
+
+const LoginForm = () => {
+    const {t} = useTranslation();
+
+
+
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+        const target = e.target;
+        const value = target instanceof HTMLInputElement && target.type === 'checkbox'
+            ? target.checked
+            : target.value;
+        const name = target.name;
+
+        setFormData(prev => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        try {
+            const apiUrl = import.meta.env.VITE_API_URL;
+            const response = await axios.post(`${apiUrl}/api/v1/main/`, formData);
+            console.log('Success:', response.data);
+            alert(t("ftr.frm.success_msg"));
+            setFormData({
+                username: '',
+                password: ''
+            });
+        } catch (error: Error | unknown) {
+            console.error('Error:', error);
+            alert(t("ftr.frm.error_msg"));
+        }
+    };
+
+    return (
+        <div className='contact-form-section'>
+            <div className="container">
+                <h1>Login Form</h1>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="username">Username</label>
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleChange}
+                        required
+                    />
+
+                    <label htmlFor="email">Password*</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                    />
+                    <button type="submit">{t("ftr.frm.btn")}</button>
+                </form>
+            </div>
+        </div>
+    );
+};
 
 export default LoginForm;
--- client/src/components/SignupForm/SignUpForm.tsx
+++ client/src/components/SignupForm/SignUpForm.tsx
@@ -1,25 +1,158 @@
-import './SignUpForm.css';
-import { useTranslation } from "react-i18next";
+import './SignUpForm.css'
+import {useState} from "react";
+import axios from "axios";
+import {useTranslation} from "react-i18next";
+import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from "react-datepicker";
 
 
 
+interface SignUpForm {
+    username: string;
+    email: string;
+    password: string;
+    password_condition:string;
+    birth_date:string;
+    accept_terms: boolean
 
-
-
-function SignUpForm() {
-
-    const {t} = useTranslation()
-    return (
-        <>
-            <form>
-                <h1>Form Sign UP</h1>
-            </form>
-        
-        
-        
-        </>
-    )
 }
 
+
+const SignUpForm = () => {
+    const {t} = useTranslation();
+
+
+
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+        password: "",
+        password_condition:"",
+        birth_date:"",
+        accept_terms:false
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+        const target = e.target;
+        const value = target instanceof HTMLInputElement && target.type === 'checkbox'
+            ? target.checked
+            : target.value;
+        const name = target.name;
+
+        setFormData(prev => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        try {
+            const apiUrl = import.meta.env.VITE_API_URL;
+            const response = await axios.post(`${apiUrl}users/api/v1/signup/`, formData);
+            console.log('Success:', response.data);
+            alert(t("ftr.frm.success_msg"));
+            setFormData({
+                username: "",
+                email: "",
+                password: "",
+                password_condition:"",
+                birth_date:"",
+                accept_terms:false
+            });
+        } catch (error: Error | unknown) {
+            console.error('Error:', error);
+            alert(t("ftr.frm.error_msg"));
+        }
+    };
+
+    return (
+        <div className='signup-form-section'>
+            <div className="container">
+                <h1>Sign Up Form</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className='row_field'>
+                        <label htmlFor="username">Username</label>
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            value={formData.username}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    
+
+                    <div className='row_field'>
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    
+
+                    <div className='row_field'>
+                        <label htmlFor="email">Birthday</label>
+                        <DatePicker
+                        aria-label='BD'
+                        value='Select birthday'
+                        selected={formData.birth_date ? new Date(formData.birth_date) : null}
+                        onChange={(date: Date | null) => {
+                            if (date) {
+                                setFormData({
+                                    ...formData, 
+                                    birth_date: date.toISOString().split('T')[0]
+                                });
+                            }   
+                        }}
+                        dateFormat="yyyy-MM-dd"
+                        maxDate={new Date()} 
+                        showYearDropdown 
+                        scrollableYearDropdown
+                        required
+                    />
+                    </div>
+                    
+
+                    <div className='row_field'>
+                        <label htmlFor="email">Password*</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            required
+                        />
+
+                    </div>
+                    
+
+                    <div className='row_field'>
+                        <label htmlFor="pass_confirm">Condition password</label>
+                        <input
+                            type="password"
+                            id="password_condition"
+                            name="password_condition"
+                            value={formData.password_condition}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    
+                    <button type="submit">{t("ftr.frm.btn")}</button>
+                </form>
+            </div>
+        </div>
+    );
+};
 
 export default SignUpForm;
