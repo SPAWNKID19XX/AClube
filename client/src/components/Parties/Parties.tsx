50184b3a70ab33a7f08ce245d0ba50d3a258467b
boris-isac <borisisac@gmail.com>
2026-01-05 04:16:42 +0000

    [ADD] party_list to frontend

--- client/src/components/Parties/Parties.tsx
+++ client/src/components/Parties/Parties.tsx
@@ -1,9 +1,72 @@
 import './Parties.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useTranslation } from "react-i18next";
+
+interface Party {
+    id: number;
+    title: string;
+    max_invited: number,
+    country: string,
+    region: string,
+    city: string,
+    address: string,
+    zip_code: string,
+    getting_places_1: string,
+    getting_places_2?: string,
+    getting_places_3?: string,
+    getting_places_4?: string,
+    getting_places_5?: string,
+    description: string,
+    start_dt: string,
+    finish_dt: string,
+    created_by: number,
+    ghosts: number[]
+}
 
 function Parties() {
+    const [parties, setParties] = useState<Party[]>([])
+    const {i18n} = useTranslation();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const parties_list = await axios.get<Party[]>('http://127.0.0.1:8000/parties/api/v1/party-list/')
+            setParties(parties_list.data)           
+        }
+
+        fetchData();
+
+        return () => {
+            
+        };
+    }, []);
+
+
     return (
         <>
-            <h1>Parties</h1>
+            <div>
+                <h1>Partie List</h1>
+                <div className="party-list">
+                    {parties.map((party) => (
+                        <div key={party.id} style={{ border: '1px solid #ccc', margin: '10px', padding: '10px' }}>
+                            <h2>{party.title}</h2>
+                            <p>{party.description}</p>
+                            <small>City: {party.city} | Max ghosts: {party.max_invited} 
+                                <p>
+                                    Date:  {new Date(party.start_dt).toLocaleString(i18n.language, {
+                                        day: 'numeric',
+                                        month: 'long',
+                                        year: 'numeric',
+                                        hour: '2-digit',
+                                        minute: '2-digit'
+                                    })}
+                                </p>
+                            </small>
+                            <button className='join_to_party'>Join</button>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </>
     )
 };
--- server/parties/models.py
+++ server/parties/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Parties(models.Model):
+    class Country(models.TextChoices):
+        PT = ('PT', "PT")
+        ES = ('ES', "ES")
 
     class Meta:
         verbose_name = "Party"
@@ -12,7 +15,7 @@ class Parties(models.Model):
     created_by=models.ForeignKey(get_user_model(),on_delete=models.CASCADE, related_name="created_by")
     title = models.CharField(max_length=100)
     max_invited = models.IntegerField()
-    country = models.CharField(max_length=2, help_text="ISO code (e.g. PT or ES)")
+    country = models.CharField(max_length=2,choices=Country ,help_text="ISO code (e.g. PT or ES)")
     region = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     address = models.CharField(max_length=200)
